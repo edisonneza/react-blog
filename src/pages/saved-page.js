f1f8a7d39997076e4a50eeab7fa2827164c0801c
eNezaLaptop <edisonneza@hotmail.com>
2021-01-15 16:24:13 +0100

    Working on SavedPage

--- src/pages/saved-page.js
+++ src/pages/saved-page.js
@@ -1,16 +1,65 @@
-import React from "react";
+import React, { useEffect, useContext, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import { TextField, Grid, Divider } from "@material-ui/core";
+import Posts from "../components/home/posts-component";
+import { GetValue } from "../services/storageService";
 
 const useStyles = makeStyles({
   root: {},
+  gridContainer: {
+    display: "flex",
+    alignItems: "center",
+  },
 });
 
 export default function SavedPage() {
   const classes = useStyles();
 
+  const [searchVal, setSearchVal] = useState("");
+  const [posts, setPosts] = useState();
+
+  useEffect(() => {
+    if(searchVal.length > 2){
+      const posts = GetValue("savedPost");
+      if(posts){
+        const postsFound = posts.filter(item => item.title.toLowerCase().indexOf(searchVal.toLowerCase()) > -1);
+        setPosts(postsFound);
+      }
+
+    } else {
+      setPosts(GetValue("savedPost"));
+    }
+  }, [searchVal]);
+
+  const handleChange = (ev) => {
+    setSearchVal(ev.target.value);
+  };
+
   return (
     <div className={classes.root}>
-      <h4>Postimet e ruajtuara</h4>
+      {/* <h4>Kërko</h4> */}
+      <Grid container className={classes.gridContainer}>
+        <Grid item xs={false} md={3}></Grid>
+        <Grid item xs={12} md={6}>
+          <TextField
+            id="standard-full-width"
+            label="Kërkoni një postim të ruajtur"
+            style={{ margin: 8 }}
+            value={searchVal}
+            // placeholder="Shkruani një fjalë ose një grup fjalësh"
+            helperText="Me shumë se 2 karaktere"
+            fullWidth
+            margin="normal"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            onChange={handleChange}
+          />
+        </Grid>
+      </Grid>
+      <Divider />
+      <br />
+      <Grid container>{posts ? <Posts posts={posts} /> : <center><h3>Asnjë postim nuk u gjend.</h3></center>}</Grid>
     </div>
   );
 }
