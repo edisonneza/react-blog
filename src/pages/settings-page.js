b499e8520dfb760f7d118a5aeeec6ea6ff064f4b
eNezaLaptop <edisonneza@hotmail.com>
2021-01-15 17:34:13 +0100

    Added copyright in settings page ;)

--- src/pages/settings-page.js
+++ src/pages/settings-page.js
@@ -1,8 +1,18 @@
-import { Container, Box } from "@material-ui/core";
+import { Typography, Link } from "@material-ui/core";
 import React from "react";
 import SettingsForm from "../components/settings/preferences-component";
 
 export default function SettingsPage() {
-  
-  return <SettingsForm />;
+  return (
+    <>
+      <SettingsForm />
+      <br/>
+      <br/>
+      <Typography variant="caption" display="block" gutterBottom>
+        <center>
+          <Link href="https://github.com/edisonneza" target="_blank">&copy;  Edison Neza </Link>
+          </center>
+      </Typography>
+    </>
+  );
 }
