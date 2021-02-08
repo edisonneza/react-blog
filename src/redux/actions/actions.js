84322bb59ab6e3c29f55169eb7ea984dbba0949c
eNezaLaptop <edisonneza@hotmail.com>
2021-02-08 10:19:34 +0100

    Installed react-redux and added actions, reducers, store

---
+++ src/redux/actions/actions.js
@@ -0,0 +1,40 @@
+export const SET_TITLE = "SET_TITLE";
+export const SET_DARK_THEME = "SET_DARK_THEME";
+export const SET_POSTS = "SET_POSTS";
+export const SET_POST = "SET_POST";
+export const SET_CATEGORIES = "SET_CATEGORIES";
+export const SET_TAGS = "SET_TAGS";
+export const SET_TAB_SELECTED = "SET_TAB_SELECTED";
+export const SET_SEARCH_POSTS = "SET_SEARCH_POSTS";
+
+export function setTitle(title) {
+  return { type: SET_TITLE, title: title };
+}
+
+export function setDarkTheme(darkTheme) {
+  return { type: SET_DARK_THEME, darkTheme };
+}
+
+export function setPosts(posts) {
+  return { type: SET_POSTS, posts };
+}
+
+export function setPost(post) {
+  return { type: SET_POST, post };
+}
+
+export function setCategories(categories) {
+  return { type: SET_CATEGORIES, categories };
+}
+
+export function setTags(tags) {
+  return { type: SET_TAGS, tags };
+}
+
+export function setTabSelected(tabSelected) {
+  return { type: SET_TAB_SELECTED, tabSelected };
+}
+
+export function setSearchPosts(posts) {
+  return { type: SET_SEARCH_POSTS, posts };
+}
---
+++ src/redux/reducers/reducers.js
@@ -0,0 +1,84 @@
+import {
+  SET_TITLE,
+  SET_DARK_THEME,
+  SET_POST,
+  SET_POSTS,
+  SET_CATEGORIES,
+  SET_TAGS,
+  SET_SEARCH_POSTS,
+  SET_TAB_SELECTED,
+} from "../actions/actions";
+import Constants from "../../constants/constants";
+import { GetValue } from "../../services/storageService";
+
+const initialState = {
+  title: Constants.appName,
+  darkTheme: GetValue("darkTheme"),
+  posts: null,
+  categories: null,
+  tags: null,
+  post: null,
+  tabSelected: { index: 0, value: "" },
+  searchPosts: {
+    searchValue: "",
+    posts: null,
+  },
+};
+
+function rootReducer(state = initialState, action) {
+  switch (action.type) {
+    case SET_TITLE:
+      return {
+        ...state,
+        title: action.title,
+      };
+    case SET_DARK_THEME:
+      return {
+        ...state,
+        darkTheme: action.darkTheme,
+      };
+    case SET_POST:
+      return {
+        ...state,
+        post: action.post,
+      };
+    case SET_POSTS:
+      return {
+        ...state,
+        posts: action.posts,
+      };
+    case SET_CATEGORIES:
+      return {
+        ...state,
+        categories: action.categories,
+      };
+
+    case SET_TAGS:
+      return {
+        ...state,
+        tags: action.tags,
+      };
+
+    case SET_SEARCH_POSTS:
+      return {
+        ...state,
+        searchPosts: {
+            searchValue: action.searchPosts.searchValue,
+            posts: action.searchPosts.posts
+        },
+      };
+
+      case SET_TAB_SELECTED:
+      return {
+        ...state,
+        tabSelected: {
+            index: action.tabSelected.index,
+            value: action.tabSelected.value
+        },
+      };
+    default:
+      return state;
+  }
+}
+
+export default rootReducer;
